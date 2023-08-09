1d0aaa61ed077b1b571885af287918399276a300
jrvieirapr <juliano.vieira@docente.pr.senac.br>
2023-08-08 21:31:49 -0300

    Metodo Create

--- models/Situacao.js
+++ models/Situacao.js
@@ -1,6 +1,26 @@
 //Biblioteca que vai conversar com o mongodb
+//npm install mongoose
 const mongoose = require('mongoose')
+
+const juroComposto = require('JuroComposto')
 
 //Use metodo "destructor" para extrair o Schema
 const { Schema } = mongoose
 
+//Criar uma constante que sera exportada
+//Inserir os dados do nosso banco de dados
+const situacaoSchema = new Schema({
+    nome : {
+        type: String,
+        required: true
+    }
+},{timestamps: true})
+
+//Definir modelo no mongoose
+const Situacao = mongoose.model("Situacao",situacaoSchema)
+
+//Exportar constantes
+module.exports = {
+    Situacao,
+    situacaoSchema
+}
---
+++ models/Tarefa.js
@@ -0,0 +1,33 @@
+//Importar o Moongose
+const mongoose = require('mongoose')
+
+//Importar Situacao
+const { situacaoSchema } = require('Situacao')
+//Extrair o schema
+const { Schema } = mongoose
+//Criar o modelo
+const tarefaSchema = new Schema({
+    assunto :{
+        type: String,
+        required: true
+    },
+    data :{
+        type: Date,
+        required: true
+    },
+    descricao :{
+        type: String,
+        required: true
+    },
+    contato :{
+        type: String,
+        required: true
+    }
+},{timestamps: true})
+//Definir o modelo no moongose
+const Tarefa = mongoose.model('Tarefa',tarefaSchema)
+//Exportar
+module.exports = {
+    Tarefa,
+    tarefaSchema
+}
