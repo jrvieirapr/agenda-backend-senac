a4db7134258c4ceb4d25afdadb6018dfbfbb5c8e
jrvieirapr <juliano.vieira@docente.pr.senac.br>
2023-08-15 19:27:51 -0300

    TarefaController

--- controllers/TarefaController.js
+++ controllers/TarefaController.js
@@ -7,7 +7,7 @@ const TarefaController = {
             data: req.body.data,
             descricao: req.body.descricao,
             contato: req.body.contato,
-            situacao: req.body.situacao,
+            tarefa: req.body.tarefa,
           };
           const response = await TarefaModel.create(tarefa);    
           res.status(201).json({ response, msg: "Criado com sucesso!" });
@@ -40,6 +40,48 @@ const TarefaController = {
         } catch (error) {
           console.log(error);
           res.status(400).json({ msg: "Não foi possivel executar pesquisa" });
+        }
+      },
+      update: async (req, res) => {
+        try {
+          const id = req.params.id;
+          const tarefa = {
+            assunto: req.body.assunto,
+            data: req.body.data,
+            descricao: req.body.descricao,
+            contato: req.body.contato,
+            tarefa: req.body.tarefa,
+          };
+          const tarefaAtualizada = await TarefaModel.findByIdAndUpdate(
+            id,
+            tarefa
+          );
+          if (!tarefaAtualizada) {
+            res.status(404).json({ msg: "Tarefa não encontrada!" });
+            return;
+          }
+          res
+            .status(200)
+            .json({ tarefaAtualizada, msg: "Tarefa atualizada com sucesso!" });
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      //Remover - Delete
+      delete: async (req, res) => {
+        try {
+          const id = req.params.id;
+          const tarefa = await TarefaModel.findById(id);
+          if (!tarefa) {
+            res.status(404).json({ msg: "Tarefa não encontrada!" });
+            return;
+          }
+          const tarefaDeletada= await TarefaModel.findByIdAndDelete(id);
+          res
+            .status(200)
+            .json({ tarefaDeletada, msg: "Tarefa deletada com sucesso!" });
+        } catch (error) {
+          console.log(error);
         }
       },
 };
