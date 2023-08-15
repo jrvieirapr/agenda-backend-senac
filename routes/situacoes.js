409b92b8c35ee7156297b8d8efe75351e7b419fd
jrvieirapr <juliano.vieira@docente.pr.senac.br>
2023-08-15 20:24:21 -0300

    Rotas

--- routes/situacoes.js
+++ routes/situacoes.js
@@ -1,10 +1,16 @@
 const router = require("express").Router()
 
 const situacaoController = require("../controllers/SituacaoController")
-
+//POST - Formularios de dados para salvar ou usuario e senhas
 router.route("/situacoes").post((req, res) => situacaoController.create(req, res))
+//GET - geralmente vou usar quando eu quiser fazer algum tipo de pesquisa
 router.route("/situacoes").get((req, res) => situacaoController.getAll(req, res))
 router.route("/situacoes/:id").get((req, res) => situacaoController.get(req, res))
-router.route("/situacoes/:id").delete((req, res) => situacaoController.delete(req, res))
+//PUT - utilizado para atualizar o registro
 router.route("/situacoes/:id").put((req, res) => situacaoController.update(req, res))
+//DELETE - quando eu precisar remover
+router.route("/situacoes/:id").delete((req, res) => situacaoController.delete(req, res))
+//OPTION - verificação antecipada
+
 module.exports = router;
+//export default SituacaoController
--- routes/tarefas.js
+++ routes/tarefas.js
@@ -1,10 +1,16 @@
 const router = require("express").Router()
 
 const tarefaController = require("../controllers/TarefaController")
-
+//POST - Formularios de dados para salvar ou usuario e senhas
 router.route("/tarefas").post((req, res) => tarefaController.create(req, res))
+//GET - geralmente vou usar quando eu quiser fazer algum tipo de pesquisa
 router.route("/tarefas").get((req, res) => tarefaController.getAll(req, res))
 router.route("/tarefas/:id").get((req, res) => tarefaController.get(req, res))
-router.route("/tarefas/:id").delete((req, res) => tarefaController.delete(req, res))
+//PUT - utilizado para atualizar o registro
 router.route("/tarefas/:id").put((req, res) => tarefaController.update(req, res))
+//DELETE - quando eu precisar remover
+router.route("/tarefas/:id").delete((req, res) => tarefaController.delete(req, res))
+//OPTION - verificação antecipada
+
 module.exports = router;
+//tarefaController - TarefaController
